4db2e8d0d3f63e4b7f341c367c66b4142f1d03b0
luciat10 <luciatravieso2@gmail.com>
2024-08-14 19:37:57 -0300

    AGREGUÉ LAS VALIDACIONES

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,36 @@
+document.getElementById("regBtn").addEventListener('click', function()
+{
+    let nombre = document.getElementById("nombre").value;
+    let apellido =document.getElementById("apellido").value;
+    let email = document.getElementById('email').value;
+    let password1 = document.getElementById('password1').value;
+    let password2 = document.getElementById('password2').value;
+    let terminos = document.getElementById('terminos').checked;
+
+    if (!nombre || !apellido || !email|| !password1 ||!password2 || !terminos){
+        showAlertError();
+        return;
+    }
+
+    if (password1.length < 6 || password2.length < 6) {
+        showAlertError();
+        return;
+    }
+     
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+
+    if (!terminos) {
+        showAlertError();
+        return;
+    }
+
+     // Si todas las validaciones pasan, muestra el mensaje de éxito
+     showAlertSuccess();
+});
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
